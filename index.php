4622b04893dabd957ffdaac70cda1b9ffa18e0e3
Ilham Fadhilah <ilhamfi_2701@yahoo.com>
2018-09-19 23:27:35 +0700

    Final progress

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+include_once 'koneksi.php';
 session_start();
 if(!isset($_SESSION['username'])){
   header('location: login.php');
@@ -17,8 +18,8 @@ if(!isset($_SESSION['username'])){
     <br>
     <a href="logout.php">Logout</a>
     <form action="" method="post" enctype="multipart/form-data">
-      <input type="text" name="nama" placeholder="Nama" class="">
-      <input type="text" name="nim" placeholder="NIM" class="">
+      <input type="text" name="nama" placeholder="Nama" class=""><br>
+      <input type="text" name="nim" placeholder="NIM" class=""><br>
       <select class="" name="fakultas" required>
         <option value="">-- Fakultas --</option>
         <option value="FIT">FIT</option>
@@ -28,11 +29,66 @@ if(!isset($_SESSION['username'])){
         <option value="FIF">FIF</option>
         <option value="FRI">FRI</option>
         <option value="FTE">FTE</option>
-      </select>
+      </select><br>
       <input type="radio" name="jk" value="laki-laki"><label for="jk">Laki - Laki</label>
-      <input type="radio" name="jk" value="perempuan"><label for="jk">Perempuan</label>
-      <input type="file" name="foto" accept="image/*">
+      <input type="radio" name="jk" value="perempuan"><label for="jk">Perempuan</label><br>
+      <input type="file" name="foto" accept="image/*"><br>
       <input type="submit" name="submit" value="Submit">
     </form>
+    <table border style="margin-top: 100px;">
+      <tr>
+        <th>NIM</th>
+        <th>Nama</th>
+        <th>Jenis Kelamin</th>
+        <th>Fakultas</th>
+        <th>Foto</th>
+      </tr>
+
+      <?php
+      $query = mysql_query("SELECT `nim`, `nama`, `fakultas`, `jenis_kelamin`, `foto` FROM `mahasiswa`");
+      $count_data = mysql_num_rows($query);
+      if ($count_data > 0) {
+        while ($data = mysql_fetch_array($query)) {
+      ?>
+      <tr>
+        <td><?php echo $data['nim']; ?></td>
+        <td><?php echo $data['nama']; ?></td>
+        <td><?php echo $data['fakultas']; ?></td>
+        <td><?php echo $data['jenis_kelamin']; ?></td>
+        <td><img src="../multimedia_storage/images/<?php echo $data['foto']; ?>" width="90"></td>
+      </tr>
+      <?php
+        }
+      }
+      ?>
+
+    </table>
   </body>
 </html>
+<?php
+if (isset($_POST['submit'])) {
+  $nama = isset($_POST['nama']) ? $_POST['nama'] : "";
+  $nim = isset($_POST['nim']) ? $_POST['nim'] : "";
+  $fakultas = isset($_POST['fakultas']) ? $_POST['fakultas'] : "";
+  $jk = isset($_POST['jk']) ? $_POST['jk'] : "";
+
+  // pemrosesan file
+  $file_name = $_FILES['foto']['name'];
+  $file_tmp_dir = $_FILES['foto']['tmp_name'];
+  $upload_dir = "../multimedia_storage/images/";
+
+  $upload_status = move_uploaded_file($file_tmp_dir, $upload_dir.$file_name);
+  if($upload_status){
+    $query = mysql_query("INSERT INTO `mahasiswa`(`nim`, `nama`, `fakultas`, `jenis_kelamin`, `foto`)
+                          VALUES ('$nim','$nama','$fakultas','$jk','$file_name')");
+    if($query){
+      echo "File Uploaded!";
+    } else {
+      echo "File Upload Failed";
+    }
+  } else {
+    echo "Failed moving file";
+  }
+}
+
+?>
